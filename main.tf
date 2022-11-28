1ad55092519327312f786eb1294acfe2601419c5
kainzs <55679372+kainzs@users.noreply.github.com>
2022-11-29 04:12:38 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+# Required providers configuration
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "xgro-tfstate"
+    key            = "terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+
+  required_version = ">= 1.0.11"
+}
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
